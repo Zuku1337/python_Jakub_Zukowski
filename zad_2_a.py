cfc64f22055870646debdbd7f32594234dd3b189
Zuku1337 <jakub.zukowski1@edu.uekat.pl>
2021-11-07 15:16:28 +0100

    Zadanie 2 a

---
+++ zad_2_a.py
@@ -0,0 +1,7 @@
+def output_names(matrix):
+    for name in matrix:
+        print(name)
+
+if __name__ == '__main__':
+    names = ["Jakub", "Patryk", "Ola", "Łukasz", "Paweł"]
+    output_names(names)
