2948e90a6c99212f17831a5dceca34ea18a9e5be
Zuku1337 <93867261+Zuku1337@users.noreply.github.com>
2021-11-07 15:17:12 +0100

    Delete zad_1_a.py

--- zad_1_a.py
+++
@@ -1,7 +0,0 @@
-def output_names(matrix):
-    for name in matrix:
-        print(name)
-
-if __name__ == '__main__':
-    names = ["Jakub", "Patryk", "Ola", "Łukasz", "Paweł"]
-    output_names(names)
