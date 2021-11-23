bc9da270165d72df69d94752e2d71cfd9cb28837
Zuku1999 <49487126+Zuku1999@users.noreply.github.com>
2021-11-23 21:21:14 +0100

    Poprawki

--- zad_2_a.py
+++ zad_2_a.py
@@ -2,6 +2,7 @@ def output_names(matrix):
     for name in matrix:
         print(name)
 
+
 if __name__ == '__main__':
     names = ["Jakub", "Patryk", "Ola", "Łukasz", "Paweł"]
     output_names(names)
