93ec7ee20d291f00c6580c29194109c32147cd07
Zuku1999 <49487126+Zuku1999@users.noreply.github.com>
2022-01-21 13:30:57 +0100

    Dodanie obsługi wczytania obrazków z pliku

--- ArgHelper.py
+++ ArgHelper.py
@@ -5,5 +5,6 @@ def arg_helper() -> []:
     arg_parse = argparse.ArgumentParser()
     arg_parse.add_argument("-m", "--model", default=None, help="path to Model File ")
     arg_parse.add_argument("-i", "--image", default=None, help="path to Image File ")
+    arg_parse.add_argument("-d", "--directory", default=None, help="path to Directory with Images")
     args = vars(arg_parse.parse_args())
     return args
--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from ArgHelper import arg_helper
 from OpenCVBuilder import OpenCVBuilder
+from os import walk
+
+
+def directory_photos(directory: str) -> []:
+    correction = []
+    for i in next(walk(directory), (None, None, []))[2]:
+        if i.endswith('.jpg') or i.endswith('.png'):
+            correction.append("{}".format(i))
+    return ["{}\{}".format(directory, file) for file in correction]
+
 
 if __name__ == "__main__":
     args = arg_helper()
     image_path = args["image"]
     model_path = args["model"]
+    directory_path = args["directory"]
     if image_path is not None:
         person_detector = OpenCVBuilder(model_path, image_path)
         person_detector.process()
+    elif directory_path is not None:
+        print(directory_photos(directory_path))
+        for image_path in directory_photos(directory_path):
+            person_detector = OpenCVBuilder(model_path, image_path)
+            person_detector.process()
